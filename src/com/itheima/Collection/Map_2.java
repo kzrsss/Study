8cb874a3de2b0ff5b3974f04c284582f1aafb5c1
kzrsss <117514033+kzrsss@users.noreply.github.com>
2026-04-07 19:13:22 +0800

    pull test

--- src/com/itheima/Collection/Map_2.java
+++ src/com/itheima/Collection/Map_2.java
@@ -22,5 +22,6 @@ public class Map_2 {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
